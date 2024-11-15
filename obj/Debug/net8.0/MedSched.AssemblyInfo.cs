e41f1ce9b8c6256741aab1288a1250550654404d
jtorova5 <jtorova5@gmail.com>
2024-11-15 12:32:40 -0500

    feat: adding readme

--- obj/Debug/net8.0/MedSched.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedSched.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedSched")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918dbf840d004f8399301df1f40409396929e667")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5006fa74a7e9eef11a98ae9bb64bc469fa3854fb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedSched")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedSched")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
