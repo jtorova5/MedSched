b881029d8ddd239b0df681c699097fba737327c8
jtorova5 <jtorova5@gmail.com>
2024-11-15 13:58:13 -0500

    feat: adding diagrams

--- obj/Debug/net8.0/MedSched.AssemblyInfo.cs
+++ obj/Debug/net8.0/MedSched.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedSched")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f3bbcea471d201531f329c1dcb64cc5776c185fd")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+281aeb47d0ddce99eed21c1431d5db1ce12e72bb")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedSched")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedSched")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
